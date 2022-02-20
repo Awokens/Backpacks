bbc1d51d18705c250fb9fce2cc9b9b84f2f1310d
Awokens <wandersbusiness@gmail.com>
2022-02-19 22:33:12 -0800

    forgot to register the open backpack event ;-;

--- Backpacks/src/main/java/me/awokens/project/backpack/Backpack.java
+++ Backpacks/src/main/java/me/awokens/project/backpack/Backpack.java
@@ -1,6 +1,7 @@
 package me.awokens.project.backpack;
 
 import me.awokens.project.backpack.listeners.click;
+import me.awokens.project.backpack.listeners.close;
 import me.awokens.project.backpack.listeners.open;
 import org.bukkit.ChatColor;
 import org.bukkit.event.Listener;
@@ -15,6 +16,7 @@ public final class Backpack extends JavaPlugin implements Listener {
         try {
             this.getServer().getPluginManager().registerEvents(new open(), this);
             this.getServer().getPluginManager().registerEvents(new click(), this);
+            this.getServer().getPluginManager().registerEvents(new close(), this);
         } catch (NullPointerException e) {
             e.printStackTrace();
             this.getPluginLoader().disablePlugin(this);
