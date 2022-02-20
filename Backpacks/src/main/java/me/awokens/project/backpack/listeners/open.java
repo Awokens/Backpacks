2ce7bce413586993f9a32ea450ccf1336b2a0161
Awokens <wandersbusiness@gmail.com>
2022-02-19 22:22:38 -0800

    added a catch in-case of failure of opening shulker box on hand

--- Backpacks/src/main/java/me/awokens/project/backpack/listeners/open.java
+++ Backpacks/src/main/java/me/awokens/project/backpack/listeners/open.java
@@ -3,7 +3,7 @@ package me.awokens.project.backpack.listeners;
 import de.tr7zw.nbtapi.NBTCompound;
 import de.tr7zw.nbtapi.NBTItem;
 import de.tr7zw.nbtapi.NBTListCompound;
-import org.bukkit.Bukkit;
+import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -23,21 +23,27 @@ public class open implements Listener {
         Player player = event.getPlayer();
         if (!player.isSneaking()) return;
         ItemStack item = event.getItem();
-        if (item == null || !(item.getType().name().contains("Shulker"))) return;
+        if (item == null || !(item.getType() == Material.SHULKER_BOX)) return;
 
         NBTItem data = new NBTItem(item);
-        Inventory inv = Bukkit.createInventory(null, 27, "Backpack of " + player.getName());
+        Inventory inv = Bukkit.createInventory(null, 27, "Backpack of " + player.getName()); // yes I know it's deprecated, calm down nerds.
 
         if (!data.hasKey("BlockEntityTag")) {
             player.openInventory(inv);
             return;
         }
+        try {
+            player.openInventory(inv);
+            Location location = player.getLocation();
+            location.getWorld().playSound(location, Sound.BLOCK_SHULKER_BOX_OPEN, 1, 1);
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+            player.sendMessage(ChatColor.RED + "Failed to open a preview of s hulker box");
+        }
         NBTCompound items = data.getCompound("BlockEntityTag");
         for (NBTListCompound compound : items.getCompoundList("Items")) {
             inv.setItem(compound.getInteger("Slot"), NBTItem.convertNBTtoItem(compound));
         }
-        player.openInventory(inv);
-
     }
 
 }
