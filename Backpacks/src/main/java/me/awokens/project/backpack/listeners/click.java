42d13acbca2e78c23ec83b5b46461043cf2117c8
Awokens <wandersbusiness@gmail.com>
2022-02-19 21:45:40 -0800

    extended security to prevent illegal item movement

--- Backpacks/src/main/java/me/awokens/project/backpack/listeners/click.java
+++ Backpacks/src/main/java/me/awokens/project/backpack/listeners/click.java
@@ -1,6 +1,7 @@
 package me.awokens.project.backpack.listeners;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -18,16 +19,20 @@ public class click implements Listener {
     public void clickBackPack(InventoryClickEvent event) {
         if (!(event.getWhoClicked() instanceof Player player)) return;
         if (!event.getView().title().toString().contains("Backpack of " + player.getName())) return;
-        if (event.getCurrentItem() != null && event.getCurrentItem().getType().name().contains("Shulker")) {
+        /*
+        Extended security checks & allow some flexibility like dragging items, pickup all, except hotbar swapping (keystrokes) for now.
+        */
+        if (event.getCurrentItem() != null && event.getCurrentItem().getType() == Material.SHULKER_BOX) {
             event.setCancelled(true);
         }
-        if (event.getAction() == InventoryAction.PLACE_ALL || event.getAction() == InventoryAction.PICKUP_ALL) {
+        if (event.getAction() == InventoryAction.HOTBAR_SWAP || event.getHotbarButton() != -1) {
             event.setCancelled(true);
         }
-
+        if (event.getView().getTopInventory().contains(Material.SHULKER_BOX)) {
+            event.setCancelled(true);
+        }
         if (event.isCancelled()) {
             player.sendMessage(ChatColor.RED + "Illegal backpack click movement!");
         }
     }
-
 }
